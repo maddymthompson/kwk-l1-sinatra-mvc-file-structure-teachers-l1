ee670b484c2bc9cc09b6cb5511c08fff364d971d
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-07-20 17:20:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
+#make sure the girls do a practice lab before trying to fux with this 
+
 class ApplicationController < Sinatra::Base
 
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
-  end
-
-  get "/" do
-  	erb :index
-  end
+configure do
+  set :views, "app/views"
+  set :public_dir, "public"
 end
+
+get "/" do
+  return erb :index
+end
+
+post "/" do 
+  @the_fortune=get_fortune
+  @the_user=params[:user]
+  return erb :results
+end 
+
+end 
+  
+  
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
-class Model
-  # Replace with Dog clas
-end
+# this is where the Ruby code goes 
+
+def get_fortune
+    fortune = ["you will adopt a puppy today", "This year you will move to a new city", "Your left nostril will look extra hot today", "you will bring a case of core water home", "karlie kloss will come"]
+  return fortune.sample
+end 
+  
+# def get_fortune
+#   #method goes here 
+#   fortunes_array = ["You'll have a great day","Someone's going to go to the bathroom in 20 minutes", "You'll have sandwiches for the rest of your life","Jessica said no","when a ball is thrown at you, you will dive into the sand","you will win in mafia","you will be the mafia"]
+#   return fortunes_array.sample
+# end 
+
+puts get_fortune
